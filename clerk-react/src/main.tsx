8ff5d74f3d40fb037aadf4737fe8688516e5276a
parthpatel <sparthpatel18@gmail.com>
2024-08-03 03:15:55 -0400

    adding .env file and try to connect with backend

--- clerk-react/src/main.tsx
+++ clerk-react/src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
